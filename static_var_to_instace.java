31401d682d5ab6eebf1175a4b8c29b679fd0fab4
anjali-dange <anjalidange9370@gmail.com>
2026-01-12 16:24:04 +0530

    Add files via upload

---
+++ static_var_to_instace.java
@@ -0,0 +1,13 @@
+package oop.com;
+class Demo4{
+    static int num=27;
+    void anj(){
+        System.out.println(num);
+    Demo4 a1=new Demo4();
+
+}
+    public static void main(String[] args){
+        Demo4 d1=new Demo4();
+        d1.anj();
+    }
+}S
