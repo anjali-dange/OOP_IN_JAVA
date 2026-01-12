45b7c18d8fb77efec54a95231bef47f8166cabc5
anjali-dange <anjalidange9370@gmail.com>
2026-01-12 16:29:46 +0530

    Add files via upload

---
+++ method_returntype_noparam.java
@@ -0,0 +1,12 @@
+//method with returntype and no parameters
+package anjali;
+public class STUDENTS {
+int add() {
+	return 100;
+}
+public static void main(String args[]) {
+	STUDENTS s1=new STUDENTS();
+	int ans =s1.add();
+	System.out.println(ans);
+}
+}
