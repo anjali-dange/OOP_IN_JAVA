c529c95c22f566f886b96eefba94c8f5e4717f7c
anjali-dange <anjalidange9370@gmail.com>
2026-01-12 16:34:18 +0530

    Add files via upload

---
+++ method_returntype_param.java
@@ -0,0 +1,13 @@
+//method with returntype and with parmeters
+package anjali;
+public class STUDENTSSS {
+int add(int a,int b) {
+	//int a=10,b=20;
+	return a+b;
+}
+	public static void main(String[] args) {
+		STUDENTSSS s1=new STUDENTSSS();
+		int ans=s1.add(4, 9);
+		System.out.println(ans);
+	}
+}
