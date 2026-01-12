fc2313531fb61dadbcbe65703680b8a5669779fa
anjali-dange <anjalidange9370@gmail.com>
2026-01-12 16:21:21 +0530

    Add files via upload

---
+++ operator_overloading.java
@@ -0,0 +1,8 @@
+//operator overloading
+package anjali;
+class Operatoroveroading {
+    public static void main(String[] args) {
+        System.out.println(10 + 20);          // 30  → addition
+        System.out.println("Anu " + "Singh"); // Anu Singh → concatenation
+    }
+}
